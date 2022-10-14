c0a0d3bc14e84f29fa63b9019d059e5b051a8016
smokystar <101525606+smokystar@users.noreply.github.com>
2022-10-14 18:47:21 +0200

    Update bestchange_coin_list.py
    postgre credentials

--- bestchange_coin_list.py
+++ bestchange_coin_list.py
@@ -14,12 +14,7 @@ api_key = '1QrbAnjDYWcnmKoQYVn2ZSphucr4yXZtWEwUATG103rqfgJqG0VZ5kW7vdtMIS0Q'
 secret_key = 'IU08Ye3WRhrjBEZl28vA9CN3TWL2fLSEv1XMZA8kYjmASbWOPpvVwhXfF6s6WQyS'
 client = Client(api_key, secret_key)
 
-con = psycopg2.connect(user="uhwgxkaboaglce",
-                       password="75db761e1367ffdf929f791edc4dcd1a58936cbe3fa9e87c920ca16338f2374c",
-                       host="ec2-52-48-159-67.eu-west-1.compute.amazonaws.com",
-                       port="5432",
-                       database="d5ehlrsmpq329l")
-cur = con.cursor()
+from main_bc import con, cur
 
 
 # start_time = datetime.now()
